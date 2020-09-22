f90ad0887e3c6910e3c3f161a963f5c272a6fabc
Adam Carter <adm.carter@gmail.com>
2020-09-22 18:07:30 -0400

    Fix db uri

--- module3-nosql-and-document-oriented-databases/insert_titanic_mongo.py
+++ module3-nosql-and-document-oriented-databases/insert_titanic_mongo.py
@@ -7,12 +7,12 @@ import csv
 load_dotenv()
 
 
+PROTOCOL = "mongodb+srv"
 MONGO_USER = os.getenv("MONGO_USER", "OOPS")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "OOPS")
 MONGO_CLUSTERNAME = os.getenv("MONGO_CLUSTERNAME", "OOPS")
-PROTOCOL = "mongodb+srv"
 QUERY = "retryWrites=true&w=majority"
-URI = f"{QUERY}://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTERNAME}?{QUERY}"
+URI = f"{PROTOCOL}://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTERNAME}?{QUERY}"
 
 
 client = pymongo.MongoClient(URI)
