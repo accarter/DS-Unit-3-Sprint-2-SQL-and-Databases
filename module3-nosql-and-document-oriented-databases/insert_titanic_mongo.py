af8e5820af9cffb89b0316f302001e381ad20006
Adam Carter <adm.carter@gmail.com>
2020-09-22 18:05:15 -0400

    Clean up module 3

--- module3-nosql-and-document-oriented-databases/insert_titanic_mongo.py
+++ module3-nosql-and-document-oriented-databases/insert_titanic_mongo.py
@@ -6,22 +6,20 @@ import csv
 
 load_dotenv()
 
+
 MONGO_USER = os.getenv("MONGO_USER", "OOPS")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "OOPS")
 MONGO_CLUSTERNAME = os.getenv("MONGO_CLUSTERNAME", "OOPS")
-
-uri = f"mongodb+srv://{MONGO_USER}" \
-      f":{MONGO_PASSWORD}@{MONGO_CLUSTERNAME}" \
-      f"?retryWrites=true&w=majority"
-
-client = pymongo.MongoClient(uri)
-print("URI:", uri)
+PROTOCOL = "mongodb+srv"
+QUERY = "retryWrites=true&w=majority"
+URI = f"{QUERY}://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTERNAME}?{QUERY}"
 
 
+client = pymongo.MongoClient(URI)
 my_db = client.titanic_data
 passenger_table = my_db.passengers
+
 
 with open('titanic.csv') as f:
   passenger_table.insert_many(csv.DictReader(f))
 
-print(passenger_table.columns)
